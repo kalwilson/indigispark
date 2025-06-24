f49d0f3ba08e3ba2266f4941ea3f6f53bcb7b712
Kal Wilson <kalwilson.dev@gmail.com>
2025-06-24 02:20:29 -0700

    Install react-router-dom, sass. Create pages, setup pages index. Create components, data, pages, styles directories. Remove css files.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,12 @@
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+import {
+  HomePage,
+  PromptStep,
+  NameStep,
+  AestheticStep,
+  SummaryPage,
+} from './pages/index.js';
 
 function App() {
   return <></>;
---
+++ client/src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as HomePage } from './HomePage.jsx';
+export { default as PromptStep } from './PromptStep.jsx';
+export { default as NameStep } from './NameStep.jsx';
+export { default as AestheticStep } from './AestheticStep.jsx';
+export { default as SummaryPage } from './SummaryPage.jsx';
