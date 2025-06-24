d199acf3ad1c660277307cc24ba88845f17b4fb0
Kal Wilson <kalwilson.dev@gmail.com>
2025-06-24 02:36:27 -0700

    Create routes dir, a routes.js, and an index for routes. Add routes to App.jsx.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
+import { routes } from './routes';
 import {
   HomePage,
   PromptStep,
@@ -9,7 +10,15 @@ import {
 } from './pages/index.js';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        {routes.map((route) => (
+          <Route key={route.path} path={route.path} element={route.element} />
+        ))}
+      </Routes>
+    </>
+  );
 }
 
 export default App;
